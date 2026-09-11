e1298d29d454a2fabb78e2ca3c2574f510895eee
radsungura <rukundaime@gmail.com>
2026-09-11 16:46:45 +0200

    change env

--- front/src/app/services/repo.ts
+++ front/src/app/services/repo.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Location } from '../../models/interfaces';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
